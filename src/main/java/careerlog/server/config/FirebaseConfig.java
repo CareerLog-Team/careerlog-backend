7bbaefb2f0f5373b870320360b6c7b4fc4856f54
Dae-Jeong <v4chelsea@gmail.com>
2024-04-21 15:08:45 +0900

    firebase 경로 절대 경로로 변환

--- src/main/java/careerlog/server/config/FirebaseConfig.java
+++ src/main/java/careerlog/server/config/FirebaseConfig.java
@@ -17,11 +17,12 @@ public class FirebaseConfig {
 
     @PostConstruct
     public void init() throws IOException {
-        String firebaseSdkRelativePath = "./src/main/resources/firebase.json";
+        String firebaseSdkRelativePath = "src/main/resources/firebase.json";
 
         File file = new File(firebaseSdkRelativePath);
         // Linux 상대 경로 처리 이슈로 인해, 절대 경로로 변환하여 입력
         String firebaseSdkAbsolutePath = String.valueOf(file.getAbsoluteFile());
+        log.info("firebase Path : {}", firebaseSdkAbsolutePath);
 
         FileInputStream serviceAccount = new FileInputStream(firebaseSdkAbsolutePath);
         FirebaseOptions options = new FirebaseOptions.Builder()
